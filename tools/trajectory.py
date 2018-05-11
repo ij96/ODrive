b9f1736f8c3380f38ded5a3fa5317df7605d4c4c
ij96 <ij96@users.noreply.github.com>
2018-05-11 21:46:03 +0100

    Modified trajectory.py - added ankle ODrive - minor formatting changes

--- tools/trajectory.py
+++ tools/trajectory.py
@@ -23,11 +23,8 @@ if len(ODriveSet) == 0:
     exit()
 else:
     print("Found {} ODrives\n".format(len(ODriveSet)))
-    my_drive = ODriveSet[1]
 
 #################### parameter configuration ###################################
-ENCODER_CPR = 4000
-
 for drive in ODriveSet:
     # M0
     drive.motor0.encoder.config.cpr = ENCODER_CPR
@@ -51,13 +48,14 @@ for drive in ODriveSet:
 
 #################### assign ODrives #########################################
 for drive in ODriveSet:
-	if drive.serial_number == 53232789697077:
-		right_hip_drive = drive
-	if drive.serial_number == 61977222983735:
-		left_hip_drive  = drive
+    if drive.serial_number == 53232789697077:
+        ankle_drive     = drive
+    if drive.serial_number == 61977222983735:
+        right_hip_drive = drive
+    if drive.serial_number == 61977223245879:
+        left_hip_drive  = drive
 
 #################### read-in trajectory from file ##############################
-
 right_hip_pitch_position   = []
 right_hip_slide_position   = []
 right_ankle_pitch_position = []
@@ -69,29 +67,25 @@ j = -1
 filePath = './'
 
 with open(filePath + 'traj_test.csv') as csvfile:
-	readCSV = csv.reader(csvfile, delimiter=',')
-	for row in readCSV:
-		if j > -1:
-			right_hip_pitch_position.append(float(row[0]))
-			right_hip_slide_position.append(float(row[1]))
-			right_ankle_pitch_position.append(float(row[2]))
-			left_hip_pitch_position.append(float(row[3]))
-			left_hip_slide_position.append(float(row[4]))
-			left_ankle_pitch_position.append(float(row[5]))
-		j = j + 1
+    readCSV = csv.reader(csvfile, delimiter=',')
+    for row in readCSV:
+        if j > -1:
+            right_hip_pitch_position.append(float(row[0]))
+            right_hip_slide_position.append(float(row[1]))
+            right_ankle_pitch_position.append(float(row[2]))
+            left_hip_pitch_position.append(float(row[3]))
+            left_hip_slide_position.append(float(row[4]))
+            left_ankle_pitch_position.append(float(row[5]))
+        j = j + 1
 
 print("Trajectory size: {}\n".format(j))
 
 #################### zero position #############################################
-
 print("Returning to zero position...\n")
-# my_drive.motor0.set_pos_setpoint(0.0, 0.0, 0.0)
-# my_drive.motor1.set_pos_setpoint(0.0, 0.0, 0.0)
 
-right_hip_drive.motor0.set_pos_setpoint(0.0, 0.0, 0.0)
-right_hip_drive.motor1.set_pos_setpoint(0.0, 0.0, 0.0)
-left_hip_drive.motor0.set_pos_setpoint(0.0, 0.0, 0.0)
-left_hip_drive.motor1.set_pos_setpoint(0.0, 0.0, 0.0)
+for drive in ODriveSet:
+    drive.motor0.set_pos_setpoint(0.0, 0.0, 0.0)
+    drive.motor1.set_pos_setpoint(0.0, 0.0, 0.0)
 
 #################### begin trajectory ##########################################
 
@@ -100,7 +94,7 @@ print("Running trajectory... (press Ctrl+C to stop)")
 i = 0   # counter
 
 # speed multiplier, for faster movement
-speed_multiplier = 4                  # speed multiplier
+speed_multiplier = 8                  # speed multiplier
 i_max = int(j / speed_multiplier) - 1 # max value of i, calculated based on j
                                       #   and speed multiplier
 print("Speed multiplier: {}".format(speed_multiplier))
@@ -117,15 +111,16 @@ while True:
     setpoint_left_slide = -400000.0 * left_hip_slide_position[i*speed_multiplier+1] # gear ratio: 1:4, 4*4000*4, problem with encoder
     setpoint_right_pitch = -30558.0 * right_hip_pitch_position[i*speed_multiplier+1] # gear ratio: 1:24, 1/3.1415*24*4000
     setpoint_right_slide = 400000.0 * right_hip_slide_position[i*speed_multiplier+1] # gear ratio: 1:4, 4*4000*4, problem with encoder
-    # setpoint_right_anklePitch = 64000.0 * right_ankle_pitch_position[i+1]
+    setpoint_right_anklePitch = 64000.0 * right_ankle_pitch_position[i*speed_multiplier+1]*3 # gear ratio: 1:16
+    setpoint_left_anklePitch = -64000.0 * left_ankle_pitch_position[i*speed_multiplier+1]*3 # gear ratio: 1:16
 
-    # my_drive.motor1.set_pos_setpoint(setpoint_left_slide, 0.0, 0.0)
-    # my_drive.motor0.set_pos_setpoint(setpoint_left_pitch, 0.0, 0.0)
-
+############################### Sending commands to corresponding motors ###############################################
     left_hip_drive.motor1.set_pos_setpoint(setpoint_left_slide, 0.0, 0.0)
     left_hip_drive.motor0.set_pos_setpoint(setpoint_left_pitch, 0.0, 0.0)
     right_hip_drive.motor1.set_pos_setpoint(setpoint_right_slide, 0.0, 0.0)
     right_hip_drive.motor0.set_pos_setpoint(setpoint_right_pitch, 0.0, 0.0)
+    ankle_drive.motor0.set_pos_setpoint(setpoint_left_anklePitch, 0.0, 0.0) 
+    ankle_drive.motor1.set_pos_setpoint(setpoint_right_anklePitch, 0.0, 0.0)
 
     #print("{}\t{:.5f}\t{:.5f}".format(i,setpoint_left_slide,setpoint_left_pitch))
     i = i + 1
