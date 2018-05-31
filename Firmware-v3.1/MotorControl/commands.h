0508dd72c66d63f1a583fe415ad95f62148cdb0b
ij96 <ij96@users.noreply.github.com>
2018-05-31 19:32:46 +0100

    Added Firmware folder for hip roll motors and v3.1 ODrive board

---
+++ Firmware-v3.1/MotorControl/commands.h
@@ -0,0 +1,28 @@
+#ifndef COMMANDS_H
+#define COMMANDS_H
+
+// TODO: resolve assert
+#define assert(expr)
+
+#ifdef __cplusplus
+
+#include <functional>
+#include <limits>
+#include "crc.hpp"
+
+extern "C" {
+#endif
+
+void init_communication(void);
+void communication_task(void const * argument);
+void set_cmd_buffer(uint8_t *buf, uint32_t len);
+void usb_update_thread();
+void USB_receive_packet(const uint8_t *buffer, size_t length);
+
+extern uint64_t serial_number;
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* COMMANDS_H */
